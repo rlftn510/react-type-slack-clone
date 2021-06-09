d00e76d0751a99ca1244eadb1dfad3379941bb77
류길수 <rlftn510@naver.com>
2021-06-09 20:21:47 +0900

    기본셋킹

---
+++ sleact/_sleact/client.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import App from './layouts/App';
+
+const App = () => {
+  return <div>helo</div>;
+};
+
+render(<App />, document.querySelector('#app'));
